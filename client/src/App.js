302af7435da7eb741d3610285d44c6ec3f02a887
Mamello <mamello3632@gmail.com>
2024-05-27 10:16:54 +0200

    Added the Login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Chat from "./components/Chat";
+import Login from './components/Login';
+import Chat from './components/Chat';
 
 function App() {
   return (
-    <div>
-      <Chat />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
