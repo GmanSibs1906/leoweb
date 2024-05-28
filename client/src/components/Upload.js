122cf80b4d14c6057ee0e2f57901c900e2f3652a
Gman SwiftNex <batiemc@gmail.com>
2024-05-28 10:42:01 +0200

    upload

---
+++ client/src/components/Upload.js
@@ -0,0 +1,236 @@
+import React, { useState, useEffect } from "react";
+import { loader, logo } from "./images";
+
+function Upload() {
+  const [chatHistory, setChatHistory] = useState([]);
+  const [userInput, setUserInput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  
+
+  // for image recognition
+  const [imageRequest, setImageRequest] = useState(false);
+  const [image, setImage] = useState();
+  const [imageResponse, setImageResponse] = useState();
+  const [imageError, setImageError] = useState();
+
+  // upload the pic
+  const uploadImage = async (e) => {
+    setImageResponse("");
+    const formData = new FormData();
+    formData.append("file", e.target.files[0]);
+    // console.log(e.target.files[0]);
+    setImage(e.target.files[0]);
+    e.target.value = null;
+
+    // send image to back end
+    try {
+      const options = {
+        method: "POST",
+        body: formData,
+      };
+      const response = await fetch("http://localhost:5000/upload", options);
+      const data = response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("problem uploading images: " + error.message);
+    }
+  };
+
+  // analyze uploaded pic
+  const analyzeImage = async () => {
+    setImageResponse("");
+    if (!image) {
+      setImageError("Please upload image first!!!");
+      return;
+    }
+    try {
+      const options = {
+        method: "POST",
+        body: JSON.stringify({
+          message: userInput,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const response = await fetch("http://localhost:5000/vision", options);
+      const data = await response.json();
+      // console.log(data)
+      setImageResponse(data.message.content);
+
+      setChatHistory((prevHistory) => [
+        { type: "image", image }, 
+        { type: "user", text: userInput },
+        { type: "imageResponse", text: data.message.content },
+        ...prevHistory, 
+      ]);
+
+    } catch (error) {
+      console.error(error.message);
+      setImageError("Something broke, please try again");
+    }
+  };
+
+  const sendMessage = async () => {
+    const userMessage = userInput;
+    setUserInput("");
+    setIsLoading(true);
+
+    try {
+      const response = await fetch("http://localhost:5000/api/chat", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userInput: userMessage }),
+      });
+
+      const data = await response.json();
+      const botMessage = data.response;
+
+      setChatHistory((prevHistory) => [
+        ...prevHistory,
+        { type: "user", text: userMessage },
+        { type: "bot", text: botMessage },
+      ]);
+    } catch (error) {
+      console.error("Error:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    // scrolling 2 the bottom whenever chatHistory updatez
+    const chatContainer = document.getElementById("chat-history");
+    if (chatContainer) {
+      chatContainer.scrollTop = chatContainer.scrollHeight;
+    }
+  }, [chatHistory]);
+
+  return (
+    <div className=" flex flex-col h-full ">
+      <img
+        src={logo}
+        alt="melsoft logo"
+        className=" h-[10vh] object-contain mb-auto "
+      />
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <div className="bg-white rounded-lg shadow-md p-6 w-96">
+          <h1 className="text-center text-2xl mb-4">Leo 2.0 Chatbot 🤖</h1>
+          <div id="chat-history" className="h-96 overflow-y-auto mb-4 flex flex-col">
+      { imageRequest && (
+        <div className="w-full my-[15px]">
+          {image && (
+            <img
+              className="w-full object-contain"
+              src={URL.createObjectURL(image)}
+              alt=""
+            />
+          )}
+        </div>
+      )}
+
+      {chatHistory.map((message, index) => (
+        <React.Fragment key={index}>
+          {message.type === "user" && (
+            <div className="text-left p-3 mb-2 rounded-lg bg-gray-200 mr-2">
+              {message.text}
+            </div>
+          )}
+          {message.type === "imageResponse" && (
+            <div className="text-left p-3 mb-2 rounded-lg bg-green-100 ml-2">
+              {message.text}
+            </div>
+          )}
+        </React.Fragment>
+      ))}
+    </div>
+
+          {imageRequest ? (
+            // upload image
+            <form
+              className="flex flex-col"
+              onSubmit={(e) => {
+                e.preventDefault();
+                analyzeImage();
+              }}
+            >
+              <div className=" w-full flex justify-center my-[10px] cursor-pointer ">
+                <div className="bg-[#100547] text-white px-4 py-1 rounded-md">
+                  <label className="" htmlFor="files">
+                    Upload
+                  </label>
+                  <input
+                    type="file"
+                    onChange={uploadImage}
+                    id="files"
+                    accept="image/*"
+                    hidden
+                  />
+                </div>
+              </div>
+              <div className=" flex ">
+                <input
+                  type="text"
+                  value={userInput}
+                  onChange={(e) => setUserInput(e.target.value)}
+                  className="flex-1 mr-2 p-2 border rounded-md"
+                  placeholder="Enter your message"
+                />
+                <button
+                  type="submit"
+                  className="bg-[#100547] text-white px-4 py-2 rounded-md"
+                >
+                  Ask Leo
+                </button>
+              </div>
+            </form>
+          ) : (
+            // no image
+            <form
+              className="flex flex-col"
+              onSubmit={(e) => {
+                e.preventDefault();
+                sendMessage();
+              }}
+            >
+              <div className=" text-sm my-[10px] w-full flex justify-center ">
+                <span
+                  className=" text-red-900 underline cursor-pointer mr-[5px] "
+                  onClick={() => setImageRequest(true)}
+                >
+                  upload image
+                </span>
+                to ask questions about.
+              </div>
+              <div className=" flex ">
+                <input
+                  type="text"
+                  value={userInput}
+                  onChange={(e) => setUserInput(e.target.value)}
+                  className="flex-1 mr-2 p-2 border rounded-md"
+                  placeholder="Enter your message"
+                />
+                <button
+                  type="submit"
+                  className="bg-[#100547] text-white px-4 py-2 rounded-md"
+                >
+                  Send
+                </button>
+              </div>
+            </form>
+          )}
+        </div>
+        {isLoading && (
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <img src={loader} width="150px" alt="Loading..." />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Upload;
